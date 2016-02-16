eebe51505f0e718efcbb4cb550bbaf720d0bafb4
Gonzalo S. Pla <gonzsanc@gmail.com>
2016-02-16 13:14:41 +0100

    Update Twitter_004_01_Extract_Jobs_From_Twitter.R

--- 03_Twitter_Message_Mining/Twitter_004_01_Extract_Jobs_From_Twitter.R
+++ 03_Twitter_Message_Mining/Twitter_004_01_Extract_Jobs_From_Twitter.R
@@ -4,7 +4,7 @@ library(streamR)
 # Extracts tweets from Twitter Stream and saves them in JSON format.
 # Words list taken from http://usefulenglish.ru/vocabulary/jobs-professions-occupations
 load("my_oauth_twitter.Rdata")
-filterStream(file.name = "job_offers_EN_01.json", # Save tweets in a json file
+filterStream(file.name = "job_offers_EN.json", # Save tweets in a json file
              track = c("job","jobs","job offer","jobs offer","vacant position","vacant positions",
                        "hiring","hire","work",
                        "director","deputy director","managing director","financial director","marketing director","general manager"
@@ -23,15 +23,14 @@ filterStream(file.name = "job_offers_EN_01.json", # Save tweets in a json file
                        ,"maths teachermath teachermusic teacher","engineer","technician","mechanic","builder"
                        ,"construction worker","repairer","welder","bricklayer","mason","carpenter","plumber"
                        ,"scientist","scholar","researcher","explorer","mathematician","physicist"
-                       ,"chemist","biologist","astronomer","historian","archeologist","economist","philosopher","psychologist"
-                       ,"judge","lawyer","attorney","legal adviser","police officer","policeman","traffic officer","detective"
+                       ,"chemist","biologist","astronomer","historian","archeologist","traffic officer","detective"
                        ,"guard","bodyguard","expert","specialist","analyst","consultant","adviser","computer programmer"
                        ,"computer operator","systems analyst","software specialist","web developer","web programmer","webmaster"
                        ,"web designer","pilot","flight engineer","flight navigator","flight attendant","stewardess","driver","taxi driver"
-                       ,"bus driver","truck driver","car mechanic","firefighter","librarian","farmer","tailor","model","politician"
-                       ,"priest","travel agent","hairdresser","hairstylist","barber","beautician","cosmetologist","cleaning lady"
+                       ,"bus driver","truck driver","car mechanic","firefighter","librarian","farmer","tailor","models","politic sciences"
+                       ,"travel agent","hairdresser","hairstylist","barber","beautician","cosmetologist","cleaning lady"
                        ,"cleaning woman","janitor","full-timepart-time","permanent job","temporary job","temporary worker"
-                       ,"freelance worker","freelancer","season worker"
+                       ,"freelance worker","freelancer","season worker","caretaker"
              ), 
              language = c("en")
              ,timeout = 60*60*24, #(4*60*60), # Keep connection alive for 60 seconds
